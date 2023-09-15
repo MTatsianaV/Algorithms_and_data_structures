5b0699235c4355fe9ce416aa2c0ed66417cd1c76
Tatsiana Marozava <mortatval@gmail.com>
2023-09-15 19:22:35 +0700

    Домашняя работа_семинар_3

---
+++ seminar_3.java
@@ -0,0 +1,56 @@
+class Node {
+    int val;
+    Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+}
+
+class seminar_3 {
+    Node head; // Добавлено поле head
+
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        this.head = prev; // Использование поля head вместо переменной head
+        return this.head;
+    }
+}
+
+public class Printer {
+    public static void main(String[] args) {
+        // Создаем связанный список 1 -> 2 -> 3 -> 4
+        Node head;
+        if (args.length == 0) {
+            head = new Node(1);
+            head.next = new Node(2);
+            head.next.next = new Node(3);
+            head.next.next.next = new Node(4);
+        } else {
+            head = new Node(Integer.parseInt(args[0]));
+            head.next = new Node(Integer.parseInt(args[1]));
+            head.next.next = new Node(Integer.parseInt(args[2]));
+            head.next.next.next = new Node(Integer.parseInt(args[3]));
+        }
+
+        // Сохраняем голову списка в поле класса Answer
+        seminar_3 ans = new seminar_3();
+        ans.reverse(head);
+
+        // Выводим инвертированный порядок элементов списка
+        Node current = ans.head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
